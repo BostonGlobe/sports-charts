d3a60ac6f518bde1d11f00313f46fd526c6163cd
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-09 18:50:00 -0500

    Reuse dev webpack in prod

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,9 @@
 module.exports = {
 	module: {
 		loaders: [
-            { test: /\.csv?$/, loader: 'dsv-loader' },
-            { test: /\.json$/, loader: 'json-loader' },
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/,
-				query: { presets: ['es2015'] },
-            },
+			{ test: /\.csv?$/, loader: 'dsv-loader' },
+			{ test: /\.json$/, loader: 'json-loader' },
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
 		],
 	},
 }
