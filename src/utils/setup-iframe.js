ac3c9f2c5353c2ed3e3c124c657e5d2acdb7c381
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-09 18:44:50 -0500

    use new iframe resizer code

--- src/utils/setup-iframe.js
+++ src/utils/setup-iframe.js
@@ -1,7 +1,7 @@
 import pymIframe from 'pym-iframe-resizer'
 
 export default function (cb) {
-	pymIframe.resizer()
+	pymIframe.setupPym({})
 	const pymChild = pymIframe.getPymChild()
 	pymChild.sendMessage('req-data-source', true)
 	pymChild.onMessage('res-data-source', cb)
