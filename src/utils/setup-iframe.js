2c5d32108c882ae76bf4e5e299c47a15725333c0
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-10 07:45:04 -0500

    remove padding

--- src/utils/setup-iframe.js
+++ src/utils/setup-iframe.js
@@ -1,11 +1,9 @@
 import pymIframe from 'pym-iframe-resizer'
 
 const setup = (callback) => {
-
 	const pymChild = pymIframe({})
 	pymChild.onMessage('receive-data', j => callback(JSON.parse(j)))
 	pymChild.sendMessage('request-data', true)
-
 }
 
 export default setup
