efe374b2c7faa3e19c73ca891bce6457eae50354
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-10 09:47:28 -0500

    update clean task

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp')
 const del = require('del')
+const argv = require('yargs').argv
+const chartPath = `charts/${argv.chart}`
 
 gulp.task('clean', (cb) => {
-	del.sync(['.tmp/**/*', 'dist/**/*'])
+	del.sync([`dist/${chartPath}/**/*`])
 	cb()
 })
