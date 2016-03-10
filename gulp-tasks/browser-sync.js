d471f4faf4e5730031ea45b94eeff7b4e32daa6e
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-10 07:46:32 -0500

    simplify browsersync task

--- gulp-tasks/browser-sync.js
+++ gulp-tasks/browser-sync.js
@@ -1,21 +1,17 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
-const shell = require('shelljs')
-const argv = require('yargs').argv
 
 gulp.task('browser-sync', (cb) => {
 	browserSync.init({
 		server: {
 			baseDir: './',
-			directory: true,
+			index: 'preview.html',
 		},
-		open: false,
 		notify: false,
 		ghostMode: false,
 	})
 
-	// open with query param
-	shell.exec(`open http://localhost:3000/preview.html?chart=${argv.chart}`, cb)
+	cb()
 })
 
 gulp.task('browser-sync-reload', () => browserSync.reload())
