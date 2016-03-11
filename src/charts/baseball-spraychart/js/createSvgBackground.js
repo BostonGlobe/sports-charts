8735238a97fb1890363ae7aca96a809f223642cd
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-11 14:51:18 -0500

    Draw svg elements (ballpark, infield, diamond, grid)

---
+++ src/charts/baseball-spraychart/js/createSvgBackground.js
@@ -0,0 +1,84 @@
+import { select } from 'd3-selection'
+import { scaleLinear } from 'd3-scale'
+import { arc } from 'd3-shape'
+
+const π = Math.PI
+
+const createSvg = ({ container, margins, width, height, parkSize }) => {
+
+	const { top, right, bottom, left } = margins
+
+	// create svg
+	const svg = select(container).append('svg')
+		.attr('width', width + left + right)
+		.attr('height', height + top + bottom)
+
+	const g = svg.append('g')
+		.attr('transform', `translate(${width/2 + left}, ${height + top})`)
+
+	// create parkScale
+	const parkScale = scaleLinear()
+		.domain([0, parkSize])
+		.range([0, height])
+
+	// create arc generator
+	const arcGenerator = arc()
+		.innerRadius(0)
+		.outerRadius(parkScale(parkSize))
+		.startAngle(-π/4)
+		.endAngle(π/4)
+
+	// make ballpark
+	g.append('path')
+		.attr('class', 'ballpark')
+		.attr('d', arcGenerator)
+
+	// make ballpark clip
+	g.append('clipPath')
+		.attr('id', 'ballpark-clip')
+	.append('path')
+		.attr('d', arcGenerator)
+
+	// make grid
+	g.selectAll('.grid')
+		.data([200, 300, 400])
+	.enter().append('circle')
+		.attr('class', 'grid')
+		.attr('r', parkScale)
+		.attr('cx', 0)
+		.attr('cy', 0)
+
+	// make grid ticks
+	g.selectAll('.tick')
+		.data([200, 300, 400])
+	.enter().append('text')
+		.attr('class', 'tick')
+		.attr('x', d => parkScale(Math.cos(-π/4) * d))
+		.attr('y', d => parkScale(Math.sin(-π/4) * d))
+		.attr('dx', 0)
+		.attr('dy', 10)
+		.text((d, i) => i === 1 ? `${d}ft` : d)
+
+	// make infield
+	g.append('circle')
+		.attr('class', 'infield')
+		.attr('clip-path', 'url(#ballpark-clip)')
+		.attr('r', parkScale(95))
+		.attr('cx', 0)
+		.attr('cy', -parkScale(Math.sqrt(2*Math.pow(90, 2))/2))
+
+	// make diamond
+	g.append('rect')
+		.attr('class', 'diamond')
+		.attr('width', parkScale(90))
+		.attr('height', parkScale(90))
+		.attr('x', 0)
+		.attr('y', -parkScale(90))
+		.attr('transform',
+			`rotate(-45) translate(${parkScale(10)}, -${parkScale(10)})`)
+
+	return svg
+
+}
+
+export default createSvg
--- src/charts/baseball-spraychart/js/main.js
+++ src/charts/baseball-spraychart/js/main.js
@@ -1,32 +1,48 @@
-import { point } from 'd3-scale/src/band.js'
+import { scalePoint } from 'd3-scale'
 
 import setupIframe from '../../../utils/setup-iframe'
 import { $ } from '../../../utils/dom.js'
-import createCanvas from './createCanvas.js'
-import createPark from './createPark.js'
+import createSvg from './createSvgBackground.js'
 
-const container = $('.chart-container')
 const π = Math.PI
+const container = $('.chart-container')
+const { offsetWidth } = container
 
-// create canvas element
-const canvas = createCanvas({ container })
+// setup chart margins
+const margins = { top: 10, right: 10, bottom: 10, left: 10 }
+const width = offsetWidth - margins.left - margins.right
+const height = Math.sqrt(Math.pow(offsetWidth, 2)/2) -
+	margins.top - margins.bottom
 
+// for now we will say our ballpark max distance is 500
+const parkSize = 443
+
+// start creating scales. first: zones to angles.
 // each zone is represented by one of the 26 letters
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-
-// create scales
 
 // 22 zones: π/2, or 90 deg
 // 2 zones: π/22
 // finally we rotate everything by π/4 counterclockwise
-const zoneToAngle = point()
+const zoneToAngle = scalePoint()
 	.domain(letters)
 	.range([π/4 - π/22, π/4 + 2*π/4 + π/22])
 
-// create grid
-createPark({ canvas })
+createSvg({ container, margins, width, height, parkSize })
 
-// fetch data and draw chart
+// // create canvas element
+// const canvas = createCanvas({ container })
+
+
+// import createCanvas from './createCanvas.js'
+// import createPark from './createPark.js'
+
+// // // create scales
+
+// // // create grid
+// // createPark({ canvas })
+
+// // // fetch data and draw chart
 
 
 
@@ -48,8 +64,8 @@ createPark({ canvas })
 // this gets fired when we receive data
 function handleDataLoaded(data) {
 
-	$('.chart-container .count span').innerHTML = data.length
-	$('.chart-container pre').innerHTML = JSON.stringify(data, null, 2)
+	// $('.chart-container .count span').innerHTML = data.length
+	// $('.chart-container pre').innerHTML = JSON.stringify(data, null, 2)
 
 }
 
@@ -58,7 +74,6 @@ function handleDataError(error) {
 	console.error(error)
 }
 
-// this starts the pym resizer and takes a callback.
-// the callback will fire when we receive data
-setupIframe(handleDataLoaded, handleDataError)
-
+// // // this starts the pym resizer and takes a callback.
+// // // the callback will fire when we receive data
+setupIframe({ handleDataLoaded, handleDataError })
