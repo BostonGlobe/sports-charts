84a24bc630209b0c32ce5731e86f1ff4040b4735
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-15 16:06:12 -0400

    node cb style data fetching

--- src/utils/setup-iframe.js
+++ src/utils/setup-iframe.js
@@ -1,27 +1,30 @@
 import getJSON from 'get-json-lite'
 import pymIframe from 'pym-iframe-resizer'
 
-const setup = ({ handleDataLoaded, handleDataError, transform }) => {
+const setup = ({ handleDataLoaded, transform }) => {
 
 	const pymChild = pymIframe({})
 
 	// send transformed data to chartifier
-	pymChild.onMessage('transform-data', d =>
-		pymChild.sendMessage('data-transformed', JSON.stringify(transform(d)))
-	)
+	pymChild.onMessage('transform-data', d => {
+		const json = JSON.parse(d)
+		let output
+		if (transform) transform(json, (err, result) => output = result)
+		else output = json
+		pymChild.sendMessage('data-transformed', JSON.stringify(output))
+	})
 
 	// talk to chartifier
 	pymChild.onMessage('receive-data', d => {
 		const json = JSON.parse(d)
 		if (transform) transform(json, handleDataLoaded)
-		else handleDataLoaded(json)
+		else handleDataLoaded(null, json)
 	})
 
 	pymChild.sendMessage('request-data', true)
 
 	// talk to production
-	pymChild.onMessage('receive-data-url', url =>
-		getJSON(url, handleDataLoaded, handleDataError))
+	pymChild.onMessage('receive-data-url', url => getJSON(url, handleDataLoaded))
 	pymChild.sendMessage('request-data-url', true)
 
 }
