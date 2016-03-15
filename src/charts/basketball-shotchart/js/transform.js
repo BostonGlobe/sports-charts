d88fbe8c3d9a41285c9e1e481e13ff0bec4dcfa1
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-14 20:05:12 -0400

    mimic cron

--- src/charts/basketball-shotchart/js/transform.js
+++ src/charts/basketball-shotchart/js/transform.js
@@ -1,4 +1,5 @@
 import { distanceBinSize } from './config'
+import zones from './zones'
 
 function calculateDistance(x, y) {
 	return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)).toFixed(2)
@@ -8,73 +9,8 @@ function calculateDistanceBin(distance) {
 	return Math.floor(distance / distanceBinSize)
 }
 
-function findZone({ shotX, shotY, distance }) {
-	const zones = [
-		{ name: 'paint (inside restricted area)', check: () =>
-			distance < 4 &&
-			shotY > -1.25
-		},
-		{ name: 'paint (outside restricted area)', check: () =>
-			shotY <= 16.25 &&
-			Math.abs(shotX) <= 8
-		},
-		{ name: 'mid range (left corner)', check: () =>
-			shotX >= -22 &&
-			shotX < -8 & 
-			shotY < 8.75 
-		},
-		{ name: 'mid range (right corner)', check: () =>
-			shotX > 8 &&
-			shotX <= 22 & 
-			shotY < 8.75 
-		},
-		{ name: 'mid range (left upper)', check: () =>
-			shotX >= -22 &&
-			shotX < -8 & 
-			shotY >= 8.75 &&
-			distance <= 23.75
-		},
-		{ name: 'mid range (right upper)', check: () =>
-			shotX > 8 &&
-			shotX <= 22 & 
-			shotY >= 8.75 &&
-			distance <= 23.75
-		},
-		{ name: 'mid range (middle upper)', check: () =>
-			Math.abs(shotX) <= 8 &&
-			shotY > 16.25 &&
-			distance <= 23.75
-		},
-		{ name: 'three (left corner)', check: () =>
-			shotX < -22 &&
-			shotY < 8.75 
-		},
-		{ name: 'three (right corner)', check: () =>
-			shotX > 22 &&
-			shotY < 8.75
-		},
-		{ name: 'three (left)', check: () =>
-			distance > 23.75 &&
-			distance < 27.75 &&
-			shotX < -8 &&
-			shotY > 8.75
-		},
-		{ name: 'three (middle)', check: () =>
-			distance > 23.75 &&
-			distance < 27.75 &&
-			Math.abs(shotX) <= 8 &&
-			shotY > 8.75
-		},
-		{ name: 'three (right)', check: () =>
-			distance > 23.75 &&
-			distance < 27.75 &&
-			shotX > 8 &&
-			shotY > 8.75
-		},
-		{ name: 'three (deep)', check: () => true },
-	]
-
-	return zones.filter(z => z.check())[0].name
+function findZone(args) {
+	return zones.filter(z => z.check(args))[0].name
 }
 
 export default function transform(data) {
---
+++ src/charts/basketball-shotchart/js/zones.js
@@ -0,0 +1,66 @@
+const zones = [
+	{ name: 'paint (inside restricted area)', check: ({shotX, shotY, distance}) =>
+		distance < 4 &&
+		shotY > -1.25
+	},
+	{ name: 'paint (outside restricted area)', check: ({shotX, shotY, distance}) =>
+		shotY <= 16.25 &&
+		Math.abs(shotX) <= 8
+	},
+	{ name: 'mid range (left corner)', check: ({shotX, shotY, distance}) =>
+		shotX >= -22 &&
+		shotX < -8 & 
+		shotY < 8.75 
+	},
+	{ name: 'mid range (right corner)', check: ({shotX, shotY, distance}) =>
+		shotX > 8 &&
+		shotX <= 22 & 
+		shotY < 8.75 
+	},
+	{ name: 'mid range (left upper)', check: ({shotX, shotY, distance}) =>
+		shotX >= -22 &&
+		shotX < -8 & 
+		shotY >= 8.75 &&
+		distance <= 23.75
+	},
+	{ name: 'mid range (right upper)', check: ({shotX, shotY, distance}) =>
+		shotX > 8 &&
+		shotX <= 22 & 
+		shotY >= 8.75 &&
+		distance <= 23.75
+	},
+	{ name: 'mid range (middle upper)', check: ({shotX, shotY, distance}) =>
+		Math.abs(shotX) <= 8 &&
+		shotY > 16.25 &&
+		distance <= 23.75
+	},
+	{ name: 'three (left corner)', check: ({shotX, shotY, distance}) =>
+		shotX < -22 &&
+		shotY < 8.75 
+	},
+	{ name: 'three (right corner)', check: ({shotX, shotY, distance}) =>
+		shotX > 22 &&
+		shotY < 8.75
+	},
+	{ name: 'three (left)', check: ({shotX, shotY, distance}) =>
+		distance > 23.75 &&
+		distance < 27.75 &&
+		shotX < -8 &&
+		shotY > 8.75
+	},
+	{ name: 'three (middle)', check: ({shotX, shotY, distance}) =>
+		distance > 23.75 &&
+		distance < 27.75 &&
+		Math.abs(shotX) <= 8 &&
+		shotY > 8.75
+	},
+	{ name: 'three (right)', check: ({shotX, shotY, distance}) =>
+		distance > 23.75 &&
+		distance < 27.75 &&
+		shotX > 8 &&
+		shotY > 8.75
+	},
+	{ name: 'three (deep)', check: ({shotX, shotY, distance}) => true },
+]
+
+export zones
