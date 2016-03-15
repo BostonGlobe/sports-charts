38ec8634e06a8eec564713c8a84209dea34ff4ae
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-15 17:34:37 -0400

    default font

--- src/base/js/config.js
+++ src/base/js/config.js
@@ -1,8 +1,6 @@
 import loadFont from './loadFont'
 
 loadFont([
-	{ family: 'Benton', suffix: 'regular', weight: 400 },
-	{ family: 'Benton', suffix: 'bold', weight: 600 },
 	{ family: 'Benton-Cond', suffix: 'regular', weight: 400 },
 	{ family: 'Benton-Comp', suffix: 'regular', weight: 400 },
 ])
