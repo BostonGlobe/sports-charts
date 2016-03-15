2824f44dc215130ff33507a227b2a7a38c6ea3a3
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-15 16:07:34 -0400

    update js template for new data handling

--- template/js/main.js
+++ template/js/main.js
@@ -1,15 +1,10 @@
 import setupIframe from '../../../utils/setup-iframe'
 
-// this gets fired when we receive data
-function handleDataLoaded(data) {
+// this gets fired when we receive data or an error
+function handleDataLoaded(err, data) {
 	console.log(JSON.stringify(data, null, 2))
-}
-
-// this gets fired when there is an error fetching data
-function handleDataError(error) {
-	console.error(error)
 }
 
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
-setupIframe({ handleDataLoaded, handleDataError })
+setupIframe({ handleDataLoaded })
