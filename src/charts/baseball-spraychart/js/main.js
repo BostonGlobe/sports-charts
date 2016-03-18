ef01cf5ef1df6a61a7a5cf34d85e1786596ed167
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-18 13:08:38 -0400

    Move hed handling to setup-iframe

--- src/charts/baseball-spraychart/js/main.js
+++ src/charts/baseball-spraychart/js/main.js
@@ -82,9 +82,6 @@ const handleInputChange = ({ e, uniqueDates, data }) => {
 
 }
 
-const handleNewHed = (hed) =>
-	document.querySelector('header span').textContent = hed
-
 const handleNewData = (newData, isChartbuilder) => {
 
 	// format the data (i.e. turn gamedate into Date, etc)
@@ -144,21 +141,6 @@ const handleNewData = (newData, isChartbuilder) => {
 
 }
 
-// this gets fired when we receive data
-const handleDataLoaded = (err, payload) => {
-
-	if (err) {
-		// TODO: better error handling
-		console.log("Oops. Look like we couldn't load this chart's data.")
-		return
-	}
-
-	const { hed, data, isChartbuilder } = payload
-	if (hed) handleNewHed(hed)
-	if (data) handleNewData(data, isChartbuilder)
-
-}
-
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
-setupIframe(handleDataLoaded)
+setupIframe(handleNewData)
--- src/charts/pointDifferential/js/main.js
+++ src/charts/pointDifferential/js/main.js
@@ -1,29 +1,9 @@
 import setupIframe from '../../../utils/setup-iframe'
 
-const handleNewHed = (hed) =>
-	document.querySelector('header span').textContent = hed
-
-const handleNewData = (data) => {
+const handleNewData = (data, isChartbuilder) => {
 
 	console.log(data)
 
 }
 
-// this gets fired when we receive data
-const handleDataLoaded = (err, payload) => {
-
-	if (err) {
-		// TODO: better error handling
-		console.log("Oops. Look like we couldn't load this chart's data.")
-		return
-	}
-
-	const { hed, data, isChartbuilder } = payload
-	if (hed) handleNewHed(hed)
-	if (data) handleNewData(data, isChartbuilder)
-
-}
-
-// this starts the pym resizer and takes a callback.
-// the callback will fire when we receive data
-setupIframe(handleDataLoaded)
+setupIframe(handleNewData)
