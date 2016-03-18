3a4ec4d93aa8f4a125ec8332bf90f38b503a5896
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-18 13:02:49 -0400

    Make updating hed part of chart boilerplate

--- template/js/main.js
+++ template/js/main.js
@@ -1,10 +1,11 @@
 import setupIframe from '../../../utils/setup-iframe'
 
-// this gets fired when we receive data or an error
-const handleDataLoaded = (err, payload) => {
-	console.log(JSON.stringify(payload, null, 2))
+const handleNewData = (data, isChartbuilder) => {
+
+	console.log(data, isChartbuilder)
+
 }
 
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
-setupIframe(handleDataLoaded)
+setupIframe(handleNewData)
