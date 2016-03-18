1338b818544aefcf395a3d0a56003376d9208968
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-18 17:03:52 -0400

    Use style guide color naming convention

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -5,11 +5,25 @@ const colors = {
 	redsox3: '#ffa7a7',
 	redsoxbg: '#ffe6e6',
 	redsoxtm: '#b83039',
+
 	gray1: '#333333',
 	gray2: '#858585',
 	gray3: '#aeaeae',
 	graybg: '#e1e1e1',
 	graypale: '#ededed',
+
+	'redsox-primary': '#ff0048',
+	'redsox-secondary': '#ff6666',
+	'redsox-tertiary': '#ffa7a7',
+	'redsox-bg': '#ffe6e6',
+	'redsox-team': '#b83039',
+
+	'gray-primary': '#333333',
+	'gray-secondary': '#858585',
+	'gray-tertiary': '#aeaeae',
+	'gray-bg': '#e1e1e1',
+	'gray-pale': '#ededed',
+
 	text: '#333333',
 
 }
