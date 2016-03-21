92a91da874071e00eb1e3fd71b1fd26c89606287
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-21 09:03:05 -0400

    proper test data file

--- src/charts/basketball-shotchart/js/main.js
+++ src/charts/basketball-shotchart/js/main.js
@@ -1,6 +1,5 @@
 import setupIframe from '../../../utils/setup-iframe'
 import chart from './chart'
-import transform from './transform'
 
 chart.setup()
 
@@ -9,7 +8,6 @@ function handleDataLoaded(err, data) {
 	if (err) console.log(err)
 	else chart.updateData(data)
 }
-
 
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
