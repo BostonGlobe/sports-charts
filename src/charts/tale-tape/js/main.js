3aa4b2d948b409b36b12037bf45b1f75c59d7aae
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-21 17:13:27 -0400

    Implement tale of the tape chart

---
+++ src/charts/tale-tape/js/main.js
@@ -0,0 +1,52 @@
+import setupIframe from '../../../utils/setup-iframe'
+import { $, addClass } from '../../../utils/dom.js'
+
+const handleNewPayload = (err, payload) => {
+
+	if (err) {
+		// TODO: better error handling
+		console.log("Oops. Look like we couldn't load this chart's data.")
+		return
+	}
+
+	const { data, timeframe, sport } = payload
+
+	// convenience variables
+	const container = $('.chart-container')
+
+	addClass(container, sport)
+	addClass($('header'), sport)
+
+	// populate hed
+	$('header h1.players').innerHTML = data[0].values.map(p =>
+		`<a href='http://www.google.com' target='_blank'>${p.player}</a>`
+	).join(' vs ')
+
+	$('header h1.timeframe span').innerHTML = timeframe
+
+	// populate bars
+	container.innerHTML = data.map(d => {
+
+		const values = d.values.map(p => p.value).sort((a, b) => a - b)
+		const max = values[1]
+
+		return `
+		<li class='measure'>
+
+			<p class='name'><span>${d.measure}</span></p>
+			<ul class='values'>
+				${d.values.map(p =>
+					`<li style='width: ${Math.round(100*p.value/max)}%'>
+						<span>${p.value}<span>
+					</li>`
+				).join('')}
+			</ul>
+
+		<li>
+		`
+
+	}).join('')
+
+}
+
+setupIframe(handleNewPayload)
