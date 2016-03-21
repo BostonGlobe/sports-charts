2ce497f1db62755888822a338f82c8d56e4f2877
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-21 09:14:03 -0400

    conform to new style

--- src/charts/basketball-shotchart/js/main.js
+++ src/charts/basketball-shotchart/js/main.js
@@ -3,12 +3,12 @@ import chart from './chart'
 
 chart.setup()
 
-// this gets fired when we receive data
-function handleDataLoaded(err, data) {
-	if (err) console.log(err)
-	else chart.updateData(data)
+const handleNewData = (data, isChartbuilder) => {
+
+	chart.updateData(data)
+
 }
 
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
-setupIframe(handleDataLoaded)
+setupIframe(handleNewData)
