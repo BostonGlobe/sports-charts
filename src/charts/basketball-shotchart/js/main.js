458edcb984f5310133b60de95ac40e26ad4ecdac
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-21 17:33:47 -0400

    Make shotchart handle hed itself

--- src/charts/basketball-shotchart/js/main.js
+++ src/charts/basketball-shotchart/js/main.js
@@ -9,6 +9,21 @@ const handleNewData = (data, isChartbuilder) => {
 
 }
 
+const handleNewPayload = (err, payload) => {
+
+	if (err) {
+		// TODO: better error handling
+		console.log("Oops. Look like we couldn't load this chart's data.")
+		return
+	}
+
+	const { data, hed, isChartbuilder } = payload
+
+	if (hed) document.querySelector('header h1').textContent = hed
+	if (data) handleNewData(data, isChartbuilder)
+
+}
+
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
-setupIframe(handleNewData)
+setupIframe(handleNewPayload)
