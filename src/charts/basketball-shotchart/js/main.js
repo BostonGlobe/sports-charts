0354f9fe495ed16800b5408f26dd02851b02bab2
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-24 16:16:23 -0400

    use promises and refactor pym setup

--- src/charts/basketball-shotchart/js/main.js
+++ src/charts/basketball-shotchart/js/main.js
@@ -1,32 +1,13 @@
 import setupIframe from '../../../utils/setup-iframe'
 import chart from './chart'
 
-let entered = false
-let storedData = {}
 chart.setup()
 
-const updateChart = ({ averages, rows }) => {
-	chart.updateData({ averages, shots: rows })
-}
-
 const handleNewData = ({ averages, rows }, isChartbuilder) => {
-	console.log('data')
-	const update = isChartbuilder || entered
-	if (update && rows) {
-		updateChart({ averages, rows })
-	} else {
-		// save data update for when enter has occured
-		storedData = { averages, rows }
-	}
+	if (rows) chart.updateData({ averages, rows }, isChartbuilder)
 }
 
-const handleNewPayload = (err, payload) => {
-	if (err) {
-		// TODO: better error handling
-		console.log("Oops. Look like we couldn't load this chart's data.")
-		return
-	}
-
+const handleNewPayload = (payload) => {
 	const { data, hed, isChartbuilder } = payload
 
 	if (hed) document.querySelector('header h1').textContent = hed
@@ -34,12 +15,6 @@ const handleNewPayload = (err, payload) => {
 
 }
 
-const handleEnterView = () => {
-	console.log('entered')
-	entered = true
-	if (storedData.rows) updateChart(storedData)
-}
-
 // this starts the pym resizer and takes a callback.
 // the callback will fire when we receive data
-setupIframe({ handleNewPayload, handleEnterView })
+setupIframe({ handleNewPayload })
--- src/utils/setup-iframe.js
+++ src/utils/setup-iframe.js
@@ -1,22 +1,61 @@
 import getJSON from 'get-json-lite'
 import pymIframe from 'pym-iframe-resizer'
+import { parse } from 'query-string'
 
-const setup = ({ handleNewPayload, handleEnterView }) => {
+const chartbuilder = ({ pymChild, handleNewPayload }) => {
+	// listen to chartifier for data
+	pymChild.onMessage('receive-data', d => {
+		const data = { ...d, isChartbuilder: true }
+		handleNewPayload(data)
+	})
+	pymChild.sendMessage('request-data', true)
+}
+
+const dev = ({ pymChild, handleNewPayload }) => {
+	const enterViewPromise = new Promise((resolve) =>
+		pymChild.onMessage('enter-view', resolve)
+	)
+	const handleNewPayloadPromise = new Promise((resolve) =>
+		pymChild.onMessage('receive-data', d => resolve(JSON.parse(d)))
+	)
+
+	Promise.all([enterViewPromise, handleNewPayloadPromise])
+		.then(value => handleNewPayload(value.pop()))
+		.catch(err => console.error(err))
+
+	pymChild.sendMessage('request-data', true)
+}
+
+const prod = ({ pymChild, handleNewPayload }) => {
+	const enterViewPromise = new Promise((resolve) =>
+		pymChild.onMessage('enter-view', resolve)
+	)
+	const handleNewPayloadPromise = new Promise((resolve) =>
+		pymChild.onMessage('receive-data-url', url => {
+			getJSON(url, (err, response) => {
+				resolve(response)
+			})
+		})
+	)
+
+	Promise.all([enterViewPromise, handleNewPayloadPromise])
+		.then(value => handleNewPayload(value.pop()))
+		.catch(err => console.error(err))
+
+	pymChild.sendMessage('request-data-url', true)
+}
+
+const setup = ({ handleNewPayload }) => {
 
 	const pymChild = pymIframe({})
 
-	// talk to chartifier
-	pymChild.onMessage('receive-data', d =>
-		handleNewPayload(null, JSON.parse(d)))
-	pymChild.sendMessage('request-data', true)
+	// determine environment (chartbuilder, dev, prod [default])
+	const parsed = parse(window.location.search)
+	const env = parsed.env || 'prod'
 
-	// talk to production
-	pymChild.onMessage('receive-data-url', url =>
-		getJSON(url, handleNewPayload))
-	pymChild.sendMessage('request-data-url', true)
-
-	// handle iframe entering view
-	pymChild.onMessage('enter-view', handleEnterView)
+	if (env === 'chartbuilder') chartbuilder({ pymChild, handleNewPayload })
+	else if (env === 'dev') dev({ pymChild, handleNewPayload })
+	else prod({ pymChild, handleNewPayload })
 
 }
 
