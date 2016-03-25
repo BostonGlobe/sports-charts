44bfc2da4e1b46a7426886077fa2f0ea27b7c491
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-25 10:19:37 -0400

    remove error handling from main.js tempalte

--- template/js/main.js
+++ template/js/main.js
@@ -1,13 +1,7 @@
 import setupIframe from '../../../utils/setup-iframe'
 
-const handleNewPayload = (err, payload) => {
-
-	if (err) {
-		// TODO: better error handling
-		console.log("Oops. Look like we couldn't load this chart's data.")
-		return
-	}
-
+const handleNewPayload = (payload) => {
+	console.log(payload)
 }
 
 // this starts the pym resizer and takes a callback.
