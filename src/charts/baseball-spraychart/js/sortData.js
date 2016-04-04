9179a7c06525a7a1ebd66ab0ce6e374f57d07f6a
Gabriel Florit <gabrielflorit@gmail.com>
2016-04-04 16:30:39 -0400

    Fix bug in baseball-spraychart sorting

--- src/charts/baseball-spraychart/js/sortData.js
+++ src/charts/baseball-spraychart/js/sortData.js
@@ -1,5 +1,5 @@
 // sort by gamedate, ascending
 const sortData = (data) =>
-	data.map(x => x).sort((a, b) => a.gamedate < b.gamedate)
+	data.map(x => x).sort((a, b) => a.gamedate - b.gamedate)
 
 export default sortData
