474a9f3f36fab4f0c3718d8c653183d5e989d064
Gabriel Florit <gabrielflorit@gmail.com>
2016-05-11 19:15:36 -0400

    Make previous-ten pass WCAG test

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,17 +1,5 @@
 const colors = {
 
-	redsox1: '#ff0048',
-	redsox2: '#ff4646',
-	redsox3: '#ffa7a7',
-	redsoxbg: '#ffe6e6',
-	redsoxtm: '#b83039',
-
-	gray1: '#333333',
-	gray2: '#858585',
-	gray3: '#8e8e8e',
-	graybg: '#e1e1e1',
-	graypale: '#ededed',
-
 	'redsox-primary': '#ff0048',
 	'redsox-secondary': '#ff4646',
 	'redsox-tertiary': '#ffa7a7',
@@ -19,8 +7,8 @@ const colors = {
 	'redsox-team': '#b83039',
 
 	'gray-primary': '#333333',
-	'gray-secondary': '#858585',
-	'gray-tertiary': '#8e8e8e',
+	'gray-secondary': '#757575',
+	'gray-tertiary': '#707070',
 	'gray-bg': '#e1e1e1',
 	'gray-pale': '#ededed',
 
