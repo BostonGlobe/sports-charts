b14dcc46cd03fe4f8db4c710df130351d5029171
Gabriel Florit <gabrielflorit@gmail.com>
2016-05-16 13:29:32 -0400

    Re-enable slider tooltip

--- src/utils/slider/setSliderTooltip.js
+++ src/utils/slider/setSliderTooltip.js
@@ -1,22 +1,22 @@
 const setSliderTooltip = ({ container, text, index }) => {
 
-// 	const input = container.querySelector('input')
-// 	const tooltip = container.querySelector('.tooltip')
+	const input = container.querySelector('input')
+	const tooltip = container.querySelector('.tooltip')
 
-// 	const min = +input.getAttribute('min')
-// 	const max = +input.getAttribute('max')
+	const min = +input.getAttribute('min')
+	const max = +input.getAttribute('max')
 
-// 	// set tooltip content
-// 	// eslint-disable-next-line no-param-reassign
-// 	tooltip.querySelector('span').innerHTML = text
+	// set tooltip content
+	// eslint-disable-next-line no-param-reassign
+	tooltip.querySelector('span').innerHTML = text
 
-// 	const position = 100 * index / (max - min)
+	const position = 100 * index / (max - min)
 
-// 	// eslint-disable-next-line no-param-reassign
-// 	tooltip.style.left = `${position}%`
+	// eslint-disable-next-line no-param-reassign
+	tooltip.style.left = `${position}%`
 
-// 	// eslint-disable-next-line no-param-reassign
-// 	tooltip.style.transform = `translateX(-${position}%)`
+	// eslint-disable-next-line no-param-reassign
+	tooltip.style.transform = `translateX(-${position}%)`
 
 }
 
