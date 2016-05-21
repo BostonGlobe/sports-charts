8e7a91545f97bf742fc55dba97b74396337f19cc
Gabriel Florit <gabrielflorit@gmail.com>
2016-05-20 22:29:24 -0400

    Update template main.js

--- template/js/main.js
+++ template/js/main.js
@@ -1,8 +1,15 @@
 import { setupIframe } from '../../../utils/setup-iframe'
 
 const handleNewPayload = (payload) => {
-	console.log(payload)
-	const { rows } = payload
+	console.log(JSON.stringify({ handleNewPayload: payload }, null, 2))
 }
 
-setupIframe(handleNewPayload)
+const handleEnterView = (payload) => {
+	console.log(JSON.stringify({ handleEnterView: payload }, null, 2))
+}
+
+const resizeEvent = (width) => {
+	console.log(JSON.stringify({ resizeEvent: width }, null, 2))
+}
+
+setupIframe({ handleNewPayload, handleEnterView, resizeEvent })
